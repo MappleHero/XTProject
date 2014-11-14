aaf0908f2e5aa48134ddecb235593d5d242b9ad7
XiaBin <xiabin@tuniu.com>
2014-11-14 13:47:10 +0800

    调整目录结构

---
+++ XTrain/XTrain/XTNetwork/XTNetwork.h
@@ -0,0 +1,11 @@
+//
+//  XTNetwork.h
+//  XTrain
+//
+//  Created by Ben on 14/11/14.
+//  Copyright (c) 2014年 XTeam. All rights reserved.
+//
+
+#import "XTNetworkEngine.h"
+#import "XTNetworkRequest.h"
+#import "XTNetworkResponse.h"
